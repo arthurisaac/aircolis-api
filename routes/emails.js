f622bbbaf0d790627b22c0e2264f90f2c8deec24
Arthur Sondo <r.thur.light@gmail.com>
2021-05-19 19:06:22 +0000

    add email option

--- routes/emails.js
+++ routes/emails.js
@@ -53,4 +53,87 @@ router.post('/welcome', function(req, res) {
 
 });
 
+router.post('/request', function(req, res) {
+
+    const email = req.body.email;
+    const defaultClient = SibApiV3Sdk.ApiClient.instance;
+    const apiKey = defaultClient.authentications['api-key'];
+    apiKey.apiKey = process.env.SENDINBLUE;
+
+    const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+    // Sender
+    let sender = SibApiV3Sdk.SendSmtpEmailSender();
+    sender = {
+        name: "Aircolis",
+        email: "r.thur.light@gmail.com"
+    };
+
+    let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+    sendSmtpEmail = {
+        sender: sender,
+        to: [{
+            email: email,
+            name: 'Arthur'
+        }],
+        templateId: 2,
+        headers: {
+            "Content-Type":"text/html",
+            "charset":"utf-8"
+        }
+    };
+
+    apiInstance.sendTransacEmail(sendSmtpEmail).then(function(data) {
+        console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+        res.json(data);
+    }, function(error) {
+        console.error(error);
+        res.end(JSON.stringify(error));
+    });
+
+
+});
+
+router.post('/accepted', function(req, res) {
+
+    const email = req.body.email;
+
+    const defaultClient = SibApiV3Sdk.ApiClient.instance;
+    const apiKey = defaultClient.authentications['api-key'];
+    apiKey.apiKey = process.env.SENDINBLUE;
+
+    const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+    // Sender
+    let sender = SibApiV3Sdk.SendSmtpEmailSender();
+    sender = {
+        name: "Aircolis",
+        email: "r.thur.light@gmail.com"
+    };
+
+    let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+    sendSmtpEmail = {
+        sender: sender,
+        to: [{
+            email: email,
+            name: 'Arthur'
+        }],
+        templateId: 3,
+        headers: {
+            "Content-Type":"text/html",
+            "charset":"utf-8"
+        }
+    };
+
+    apiInstance.sendTransacEmail(sendSmtpEmail).then(function(data) {
+        console.log('API called successfully. Returned data: ' + data);
+        res.end(JSON.stringify(data));
+    }, function(error) {
+        console.error(error);
+        res.end(JSON.stringify(error));
+    });
+
+
+});
+
 module.exports = router;

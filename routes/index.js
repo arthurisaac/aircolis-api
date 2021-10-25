b348c38eefa605e82497f632e69d760983be6786
arthurisaac <r.light.light@gmail.com>
2021-10-25 16:50:49 +0000

    ajout alerte

--- routes/index.js
+++ routes/index.js
@@ -40,4 +40,39 @@ router.post('/notification', function (req, res) {
 
 });
 
+router.post('/notificationalerte', function (req, res) {
+    const token = req.body.token;
+    const postID = req.body.postID;
+
+    //console.log(req.params.token);
+    if (token) {
+        //var topic = 'general';
+        const message = {
+            notification: {
+                title: 'Aircolis',
+                body: 'Un voyage correspond à votre alerte',
+            },
+            //topic: topic
+            token: token,
+            data: {
+                "postID": postID
+            }
+        };
+        admin.messaging().send(message)
+            .then((response) => {
+                // Response is a message ID string.
+                console.log('Successfully sent message:', response);
+                res.json({"succes": "Successfully sent: " + response});
+                //res.render('index', { title: 'Successfully sent message:' + response });
+            })
+            .catch((error) => {
+                res.end({"error": error});
+                console.log('Error sending message:', error);
+            });
+    } else {
+        res.json({"error": "Aucun token"});
+    }
+
+});
+
 module.exports = router;
